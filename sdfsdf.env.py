0faef83938eb38b4954b8d8d7f7c9d21b9e4096b
NikolayGlazkov <gn9377419582@gmail.com>
2024-04-30 15:13:52 +0300

    fix

---
+++ sdfsdf.env.py
@@ -0,0 +1,7 @@
+
+SECRET_KEY = 'django-insecure-*+c4w_!&mga2#q7kujc3zvx+bydn7%cm7^(6ncgjlk)*lu29g^'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'gn9377419582@gmail.com'
+EMAIL_HOST_PASSWORD = 'wiag rmsv kjia ootn'
